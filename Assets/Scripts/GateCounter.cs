5549fcc95c34c9a4084c16264083b77b84c81595
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-16 22:02:52 +0300

    Each time a gate is passed the plane speed gets increased by 5

--- Assets/Scripts/GateCounter.cs
+++ Assets/Scripts/GateCounter.cs
@@ -6,15 +6,24 @@ using UnityEngine;
 public class GateCounter : MonoBehaviour
 {
     private int gatesPassed = 0;
+    private PlayerController playerController;
 
     public TextMeshProUGUI gateText;
     public TextMeshProUGUI announcerText;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Gate"))
         {
             gatesPassed++;
+
+            playerController.FlySpeed += 5f;
+
             gateText.text = $" {gatesPassed} / 5 Gates passed";
 
             if (gatesPassed >= 5)
