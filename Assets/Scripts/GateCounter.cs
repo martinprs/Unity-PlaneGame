c5784b33a2cbef9d6ffbfccd13e02e32a970278a
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-16 21:54:27 +0300

    Add a check for gates passed which updates announcer text

--- Assets/Scripts/GateCounter.cs
+++ Assets/Scripts/GateCounter.cs
@@ -8,6 +8,7 @@ public class GateCounter : MonoBehaviour
     private int gatesPassed = 0;
 
     public TextMeshProUGUI gateText;
+    public TextMeshProUGUI announcerText;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,6 +16,12 @@ public class GateCounter : MonoBehaviour
         {
             gatesPassed++;
             gateText.text = $" {gatesPassed} / 5 Gates passed";
+
+            if (gatesPassed >= 5)
+            {
+                announcerText.text = "You win!";
+            }
+
             Destroy(other.gameObject);
         }
     }
