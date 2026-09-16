d3aa966c506486e7ed2e6fdfe6b6c99da41db04c
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-16 21:33:07 +0300

    Update script to display the number of gates passed

--- Assets/Scripts/GateCounter.cs
+++ Assets/Scripts/GateCounter.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GateCounter : MonoBehaviour
 {
     private int gatesPassed = 0;
 
+    public TextMeshProUGUI gateText;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Gate"))
         {
             gatesPassed++;
+            gateText.text = $" {gatesPassed} / 5 Gates passed";
             Destroy(other.gameObject);
         }
     }
